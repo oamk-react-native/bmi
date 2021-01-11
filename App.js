4ba5fccb9d16eed1429324bf7f346d753966e5ff
Jouni Juntunen <jouni.juntunen@oamk.fi>
2021-01-11 20:59:21 +0200

    Finished example.

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+  const [bmi, setBmi] = useState(0);
+
+  function calculate() {
+    const heightwithdigit = height.replace(',','.');
+    const result = weight / Math.pow(heightwithdigit,2);
+    setBmi(result);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Weight</Text>
+      <TextInput style={styles.field} onChangeText={text => setWeight(text)} value={weight} keyboardType='decimal-pad'/>
+      <Text style={styles.field}>Height</Text>
+      <TextInput style={styles.field} onChangeText={text => setHeight(text)} value={height} keyboardType='decimal-pad'/>
+      <Text style={styles.field}>Bmi</Text>
+      <Text style={styles.field}>{bmi.toFixed(1)}</Text>
+      <Button onPress={calculate} title="Calculate"></Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 20,
   },
+  field: {
+    marginBottom: 10,
+  }
 });
